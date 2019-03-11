afff796afd2fa17a9e50a2444d915af2e6d8c014
rodion <rodiongork@github.com>
2019-03-11 09:34:16 +0300

    Using image for gameover

--- src/main/java/denisdino/Logic.java
+++ src/main/java/denisdino/Logic.java
@@ -2,35 +2,27 @@ package denisdino;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Logic {
 	MainAndWindow main;
 	static boolean inGame = true;
-	public static int[][] GameOver = { { 2, 4, 5, 6, 1 }, { 1, 6, 3, 8 }, { 0, 2, 4, 2, 2, 1, 6, 1 },
-			{ 0, 1, 6, 1, 2, 1, 6, 1 }, { 0, 1, 3, 1, 2, 1, 2, 1, 6, 1 }, { 0, 1, 3, 4, 2, 1, 6, 1 },
-			{ 0, 1, 3, 4, 2, 8 }, { 0, 1, 3, 4, 3, 6, 1 }, { 18 }, { 2, 6, 2, 5, 3 }, { 1, 7, 2, 6, 2 },
-			{ 0, 2, 3, 1, 9, 2, 1 }, { 0, 1, 4, 1, 10, 2 }, { 0, 2, 3, 1, 9, 2, 1 }, { 1, 7, 2, 6, 2 },
-			{ 2, 6, 2, 5, 3 }, { 18 }, { 0, 8, 2, 8 }, { 0, 8, 2, 1, 2, 2, 2, 1 }, { 1, 2, 7, 1, 2, 2, 2, 1 },
-			{ 2, 2, 6, 1, 2, 2, 2, 1 }, { 1, 2, 7, 1, 2, 2, 2, 1 }, { 0, 8, 2, 1, 2, 2, 2, 1 }, { 0, 8, 2, 1, 6, 1 },
-			{ 18 }, { 0, 8, 2, 8 }, { 0, 1, 2, 2, 2, 1, 2, 8 }, { 0, 1, 2, 2, 2, 1, 2, 1, 4, 1, 2 },
-			{ 0, 1, 2, 2, 2, 1, 2, 1, 4, 2, 1 }, { 0, 1, 2, 2, 2, 1, 2, 1, 3, 4 }, { 0, 1, 2, 2, 2, 1, 2, 5, 1, 2 },
-			{ 0, 1, 6, 1, 3, 4, 2, 1 } };
+	static BufferedImage gameOverImage;
+	
+	static {
+	    try {
+	        gameOverImage = ImageIO.read(Logic.class.getResourceAsStream("/gameover.png"));
+	    } catch (IOException e) {
+	        throw new RuntimeException(e);
+	    }
+	}
 
 	public static void printGameOver(Graphics g) {
-		for (int i = 0; i < GameOver.length; i++) {
-			int x = 0;
-			g.setColor(Color.RED);
-			for (int j = 0; j < GameOver[i].length; j++) {
-				if (j % 2 != 0) {
-					g.fillRect((int) (Settings.SCREEN_WIDTH * 0.4) + Settings.PIXEL * 2 * i,
-							(int) (Settings.SCREEN_HEIGHT * 0.3) + Settings.PIXEL * 2 * x, Settings.PIXEL * 2,
-							Settings.PIXEL * 2 * GameOver[i][j]);
-					x += GameOver[i][j];
-				} else {
-					x += GameOver[i][j];
-				}
-			}
-		}
+		g.drawImage(gameOverImage,
+				Settings.SCREEN_WIDTH / 3, Settings.SCREEN_HEIGHT / 3,
+				Settings.SCREEN_WIDTH / 3, Settings.SCREEN_HEIGHT / 3, null);
 	}
 
 	public static void ifDinoIsInBounce() {
