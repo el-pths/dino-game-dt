bf60d37e0290278d955635fe5b7050a8a40e4104
ReDBrEaD566 <48030823+ReDBrEaD566@users.noreply.github.com>
2019-04-16 16:14:55 +0300

    Update Settings.java

--- src/main/java/dinosaur/Settings.java
+++ src/main/java/dinosaur/Settings.java
@@ -3,8 +3,7 @@ package dinosaur;
 import java.awt.Toolkit;
 
 public class Settings {
-	public static final int START_WIDTH = (int) ((Toolkit.getDefaultToolkit()).getScreenSize().width * 0.7),
-			START_HEIGHT = (int) (((Toolkit.getDefaultToolkit()).getScreenSize()).height * 0.5), PIXEL = 4;
+	public static final int START_WIDTH = 1344, START_HEIGHT = 540, PIXEL = 4;
 	public static int speed = 90, width = START_WIDTH, height = START_HEIGHT, koefficient_paraboli = 3;
 	public static final int againXX = 200, againYY = 60, againX = (width - againXX) / 2, againY = height / 16;
 	public static final int closePortXX = 60, closePortYY = 60, closePortX = (int) (width * 29 / 32),
